2f4dab8e2fb131176192acd93d618e2f564bc3d2
Ernesto Cambuston <ernesto.cambuston@instacart.com>
2026-05-07 23:59:34 -0700

    repro maps

--- Repro.swift
+++ Repro.swift
@@ -1,7 +1,29 @@
-import Foundation
-import GTMAppAuth
-import GTMSessionFetcher
+import UIKit
+import SwiftUI
+import MapKit
 
-public final class Repro {
-	public let id = ObjectIdentifier(GTMSessionFetcherAuthorizer.self)
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+    }
+}
+
+var hola: some View {
+	Map(
+        coordinateRegion: .constant(MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+            latitudinalMeters: 200,
+            longitudinalMeters: 200
+        )),
+        annotationItems: [
+            MapPin(coordinate: CLLocationCoordinate2D(latitude: 0, longitude: 0))
+        ]
+    ) { pin in
+        MapMarker(coordinate: pin.coordinate, tint: .green)
+    }
 }
